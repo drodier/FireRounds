4354ce3d1dcefa359b84492489c87de4f1306cbd
drodier <davidrodier96@hotmail.com>
2023-02-24 23:57:46 -0500

    Ajouter un loader de units custom pour la gameloop

--- Assets/Scripts/UnitsManager.cs
+++ Assets/Scripts/UnitsManager.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class UnitsManager : MonoBehaviour
 {
-    public Unit[] units;
+    public Unit[] units = new Unit[2];
+    public GameObject[] unitTypes;
     public MapLogic.Tile[,] mapGrid;
 
     // Start is called before the first frame update
     void Start()
     {
-        units = FindObjectsOfType<Unit>();
-        StartIniative();
+        
     }
 
     // Update is called once per frame
@@ -24,7 +24,10 @@ public class UnitsManager : MonoBehaviour
     public void StartIniative()
     {
         Array.Sort(units, new UnitComparer());
+    }
 
+    public void DebugUnits()
+    {
         foreach(Unit unit in units)
         {
             Debug.Log(unit.name + "\n" +
@@ -32,7 +35,21 @@ public class UnitsManager : MonoBehaviour
                         "Mana : " + unit.getMana() + "/" + unit.getMaxMana() + "\n" +
                         "Initiative : " + unit.getInitiative());
         }
+    }
 
-        units[0].toggleMenu();
+    public void LoadUnits()
+    {
+        Debug.Log(units.Length + " - " + unitTypes.Length);
+        units[0] = Instantiate(unitTypes[0]).GetComponent<Unit>();
+        units[0].position = new Vector2(0,0);
+        units[0].currentTile = GameObject.Find("[0,0]").GetComponent<TileLogic>();
+        units[0].currentTile.unitOnTile = units[0];
+
+        units[1] = Instantiate(unitTypes[1]).GetComponent<Unit>();
+        units[1].position = new Vector2(3,3);
+        units[1].currentTile = GameObject.Find("[3,3]").GetComponent<TileLogic>();
+        units[1].currentTile.unitOnTile = units[1];
+
+        StartIniative();
     }
 }
