d1bfdaf998faf8d5fa7e66fff76937acb5321702
drodier <davidrodier96@hotmail.com>
2023-02-25 10:18:08 -0500

    Modifier le logic de la gameloop

--- Assets/Scripts/UnitsManager.cs
+++ Assets/Scripts/UnitsManager.cs
@@ -5,25 +5,43 @@ using UnityEngine;
 
 public class UnitsManager : MonoBehaviour
 {
+    public GameObject canvas;
+
+    public UnitCard unitCard;
     public Unit[] units = new Unit[2];
     public GameObject[] unitTypes;
     public MapLogic.Tile[,] mapGrid;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartIniative()
     {
         Array.Sort(units, new UnitComparer());
+
+        for(int i=0; i<units.Length; i++)
+        {
+            UnitCard currentCard = Instantiate(unitCard);
+            currentCard.transform.SetParent(canvas.transform, false);
+            currentCard.SetUnit(units[i]);
+            currentCard.transform.position += new Vector3(i*75, 0, 0);
+        }
+    }
+
+    public void LoadUnits()
+    {
+        canvas = GameObject.Find("UnitsOrderCanvas");
+
+        units[0] = Instantiate(unitTypes[0]).GetComponent<Unit>();
+        units[0].position = new Vector2(0,0);
+        units[0].currentTile = GameObject.Find("[0,0]").GetComponent<TileLogic>();
+        units[0].currentTile.unitOnTile = units[0];
+        units[0].name = "Mage";
+
+        units[1] = Instantiate(unitTypes[1]).GetComponent<Unit>();
+        units[1].position = new Vector2(3,3);
+        units[1].currentTile = GameObject.Find("[3,3]").GetComponent<TileLogic>();
+        units[1].currentTile.unitOnTile = units[1];
+        units[1].name = "Warrior";
+
+        StartIniative();
     }
 
     public void DebugUnits()
@@ -35,21 +53,5 @@ public class UnitsManager : MonoBehaviour
                         "Mana : " + unit.getMana() + "/" + unit.getMaxMana() + "\n" +
                         "Initiative : " + unit.getInitiative());
         }
-    }
-
-    public void LoadUnits()
-    {
-        Debug.Log(units.Length + " - " + unitTypes.Length);
-        units[0] = Instantiate(unitTypes[0]).GetComponent<Unit>();
-        units[0].position = new Vector2(0,0);
-        units[0].currentTile = GameObject.Find("[0,0]").GetComponent<TileLogic>();
-        units[0].currentTile.unitOnTile = units[0];
-
-        units[1] = Instantiate(unitTypes[1]).GetComponent<Unit>();
-        units[1].position = new Vector2(3,3);
-        units[1].currentTile = GameObject.Find("[3,3]").GetComponent<TileLogic>();
-        units[1].currentTile.unitOnTile = units[1];
-
-        StartIniative();
     }
 }
