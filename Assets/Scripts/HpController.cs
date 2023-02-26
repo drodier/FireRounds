7f35deabcd1a5f3b46a5ce122da049b2df4fbf81
drodier <davidrodier96@hotmail.com>
2023-02-25 21:35:40 -0500

    Ajuster les controller de bar de status

--- Assets/Scripts/HpController.cs
+++ Assets/Scripts/HpController.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class HpController : MonoBehaviour
 {
-    public Unit unit;
+    public GameObject unit;
     public Transform container;
-
-    void Start()
-    {
-            container.position = new Vector3(unit.position.x + (1 - (unit.getHealth() / unit.getMaxHealth())), unit.position.y + 0.85f, 0);
-    }
 
     void FixedUpdate()
     {
-        float delta = (float)unit.getHealth() / (float)unit.getMaxHealth();
+        float delta = (float)unit.GetComponent<Unit>().getHealth() / (float)unit.GetComponent<Unit>().getMaxHealth();
         transform.localScale = new Vector2(delta, 1);
         transform.position = new Vector3(container.position.x - (1-transform.localScale.x)/1.33f, container.position.y, -1);
     }
